e86972deac59744b9c60595b3e827d15c8ef75db
DESKTOP-U71M6RS\USER <thanhdd300599@gmail.com>
2021-11-09 14:48:43 +0700

    bai 1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import { useState } from "react";
 
+
+const order = [100,200,300]
+
 function App() {
+  
+  const total  = order.reduce((total, cur) => total + cur)
+
   // Khi đặt trên state là gì thì đặt thằng thứ 2 theo tên của nó là thêm set và theo quy ước Camel Case
-  const [counter, setCounter] = useState(1)
+  const [counter, setCounter] = useState(()=>{
+    const total  = order.reduce((total, cur) => total + cur)
+    console.log(total)
+    return total
+  })
   const handleIncrease = () => {
-    setCounter(counter + 1)
+    setCounter(preState => preState +1)
   }
   
   return (
     <div className="App" style={{ padding: 20 }}>
-      <h3>{counter}</h3>
+      <h1>{counter}</h1>
       <button onClick={handleIncrease}>Increase</button>
     </div>
   );
