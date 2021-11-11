c2cd86b0d407bc9fc566d82678c8ff67cc2eb46c
DESKTOP-U71M6RS\USER <thanhdd300599@gmail.com>
2021-11-11 11:46:32 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,38 +1,16 @@
+import Content from "./Content"
 import { useState } from "react";
 
 
 function App() {
+  const [show, setShow] = useState(false)
 
-  const [job, setJob] = useState('')
-  const [jobs, setJobs] = useState(() => {
-    const storageJobs = JSON.parse(localStorage.getItem('jobs'))
-    return storageJobs ?? []
-  })
 
-  const handleSubmit = () => {
-    setJobs(pre => {
-      const newJobs = [...pre, job]
-
-      // Save to local storage
-      const jsonJobs = JSON.stringify(newJobs)
-      localStorage.setItem('jobs', jsonJobs)
-      return newJobs
-    })
-
-    setJob('')
-  }
-  console.log(job);
 
   return (
     <div className="App" style={{ padding: 20 }}>
-      <input value={job} onChange={e => setJob(e.target.value)} />
-      <button onClick={handleSubmit} >Add</button>
-
-      <ul>
-        {jobs.map((job, index) => (
-          <li key={index}>{job}</li>
-        ))}
-      </ul>
+      <button  onClick  = {() => setShow(!show)} > Toggle </button>
+      {show && <Content />}
     </div>
   );
 }
